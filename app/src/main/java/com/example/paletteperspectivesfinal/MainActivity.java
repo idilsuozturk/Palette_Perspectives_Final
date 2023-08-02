6c0a21f9c246855f6a987e901405d38b6e3d94a9
idilsuozturk <99552612+idilsuozturk@users.noreply.github.com>
2023-08-02 03:43:14 +0300

    Added login page.

--- app/src/main/java/com/example/paletteperspectivesfinal/MainActivity.java
+++ app/src/main/java/com/example/paletteperspectivesfinal/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.paletteperspectivesfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button seller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seller = (Button)findViewById(R.id.seller);
+        seller.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LogInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
