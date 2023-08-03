84efd64031d3d51c4582763c899d846093d34f12
basaksu <138883064+basaksu@users.noreply.github.com>
2023-08-04 02:08:48 +0300

    Error fixing

--- app/src/main/java/com/example/paletteperspectivesfinal/MainActivity.java
+++ app/src/main/java/com/example/paletteperspectivesfinal/MainActivity.java
@@ -1,27 +1,43 @@
 package com.example.paletteperspectivesfinal;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Button seller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        seller = (Button)findViewById(R.id.seller);
-        seller.setOnClickListener(new View.OnClickListener() {
+
+        Button adminButton = findViewById(R.id.admin);
+        Button buyerButton = findViewById(R.id.buyer);
+        Button sellerButton = findViewById(R.id.seller);
+
+        adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        buyerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        sellerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, LogInActivity.class);
                 startActivity(intent);
             }
         });
+
+
     }
 }
