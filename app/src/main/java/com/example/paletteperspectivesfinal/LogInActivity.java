619e98a18aa4c8066ada0ef9d33386a3a79a8b66
idilsuozturk <99552612+idilsuozturk@users.noreply.github.com>
2023-08-04 01:52:32 +0300

    Fixed kotlin version error.

--- app/src/main/java/com/example/paletteperspectivesfinal/LogInActivity.java
+++ app/src/main/java/com/example/paletteperspectivesfinal/LogInActivity.java
@@ -1,11 +1,8 @@
 package com.example.paletteperspectivesfinal;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -13,14 +10,5 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-
-        Button loginButton = findViewById(R.id.button4);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LogInActivity.this, MainSellerActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
