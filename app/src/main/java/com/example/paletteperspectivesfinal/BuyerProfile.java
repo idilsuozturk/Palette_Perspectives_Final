6050697b7af88ea6daa70e890d25b4af22b21921
Halis Vefa Türkyılmaz <halisvefa@gmail.com>
2023-08-06 17:00:33 +0300

    Update BuyerProfile.java

--- app/src/main/java/com/example/paletteperspectivesfinal/BuyerProfile.java
+++ app/src/main/java/com/example/paletteperspectivesfinal/BuyerProfile.java
@@ -5,26 +5,56 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class BuyerProfile extends AppCompatActivity {
 
     FirebaseAuth auth;
     Button logOutButton;
     FirebaseUser user;
-
+    FirebaseFirestore fireStore;
+    TextView name;
+    String fullName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buyer_profile);
-
+        name = findViewById(R.id.textView16);
         auth = FirebaseAuth.getInstance();
         logOutButton = findViewById(R.id.logOutButton1);
         user = auth.getCurrentUser();
+        fireStore = FirebaseFirestore.getInstance();
+        String UId = user.getUid();
+
+
+        fireStore.collection("Users")
+                .whereEqualTo("ID", UId)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                name.setText(document.get("First Name").toString());
+                            }
+                        }
+                    }
+                });
+
+
+
         if (user == null) {
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
