de2558b6492f6ae56ed9d7d0ce6748287998abe4
Faruk <109393466+farukbeygo@users.noreply.github.com>
2023-08-08 20:36:48 +0300

    adjustments on offers

--- app/src/main/java/Classes/HardCopyArtPiece.java
+++ app/src/main/java/Classes/HardCopyArtPiece.java
@@ -4,18 +4,23 @@ be the super class of the Auction Art Piece class. Moreover, we implement a new 
 start an auction that creates a regular auction process like in real life.
  */
 
+/*
+package Classes;
 
-/* package Classes;
+import java.util.HashMap;
 
-public class HardCopyArtPiece extends ArtPiece {
+public class HardCopyArtPiece extends ArtPiece implements Offerable {
     private String photoUrl;
     private int price;
     private boolean canOffered;
+    private HashMap<User, Integer> offers;
 
     public HardCopyArtPiece(String title, Seller artist, int price, boolean canOffered) {
         super(title, artist);
         this.price = price;
         this.canOffered = canOffered;
+        this.offers = new HashMap<User, Integer>();
+
     }
 
     public String getPhotoUrl() {
@@ -25,4 +30,19 @@ public class HardCopyArtPiece extends ArtPiece {
     public void setPhotoUrl(String photoUrl) {
         this.photoUrl = photoUrl;
     }
+
+    @Override
+    public void addOffer(User user, int offer) {
+        if (this.canOffered) {
+            offers.put(user, offer);
+        }
+    }
+
+    @Override
+    public void removeOffer(User user) {
+        if (this.canOffered) {
+            offers.remove(user);
+        }
+    }
+
 } */
